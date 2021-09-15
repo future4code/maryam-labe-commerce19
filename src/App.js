09fc181dbd64468a3fb7efd4550a65c382ca4605
Tainá Soares <tainasoares10eler@gmail.com>
2021-09-15 18:36:01 -0300

    inicio da home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import Sputnik from './components/imgs/sputnik.jpg'
+import Vostok1 from './components/imgs/vostok1.jpg'
+import Explorer1 from './components/imgs/explorer1.jpg'
+import Luna10 from './components/imgs/luna10.jpg'
 
 function App() {
+
+  const Card = styled.div`
+   border: 1px solid black;
+   width: 400px;
+   margin: 12px;
+  `
+
+  const produtos = [
+    {
+      id:1,
+      nome:'Satélite Vostok 1',
+      preco: 20,
+      imagem: {Vostok1}
+    },
+    {
+      id:2,
+      nome:'Satélite Explorer 1',
+      preco: 20,
+      imagem:(Explorer1)
+    },
+    {
+      id:3,
+      nome:'Satélite Sputnik',
+      preco: 20,
+      imagem:(Sputnik)
+    },
+    {
+      id:4,
+      nome:'Satélite Luna 10',
+      preco: 20,
+      imagem:(Luna10)
+    }
+  ]
+  
+ const listaDeProdutos = produtos.map((cadaProduto) =>{
+   return(
+     <Card>
+       <p>{cadaProduto.imagem}</p>
+       <p>{cadaProduto.nome}</p>
+       <p>R${cadaProduto.preco}</p>
+       <button>Adicionar ao Carrinho</button>
+     </Card>
+   )
+ })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>{listaDeProdutos}</div>
   );
 }
 
