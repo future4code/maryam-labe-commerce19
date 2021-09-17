ff634a230eabedb42c0621151887a6723e6a827e
LuisH3n <85587362+LuisH3n@users.noreply.github.com>
2021-09-17 18:31:29 -0300

    adicionando últimos detalhes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import CadaCard from './CadaCard';
 
-
-
-
 class Cards extends React.Component{
     render(){
-    return(
-        < >
-            {this.props.card.map(card => (
-            <div key={card.id}>
-                <CadaCard nome={card.nome} imagem={card.imagem} preco={card.preco}/>
-            </div>))}
-        </>
-    )
- }
+        return(
+            < >
+                {this.props.card.map(card => (
+                <div key={card.id}>
+                    <CadaCard nome={card.nome} imagem={card.imagem} preco={card.preco}/>
+                </div>))}
+            </>
+        )
+    }
 }
 
 export default Cards;
